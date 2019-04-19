f5dc602b6349ab5700f10ee41a7d9e9ae052dbfd
Goran Mekić <meka@tilda.center>
2019-04-19 20:09:23 +0200

    Login to SMTP server

--- pyser/utils.py
+++ pyser/utils.py
@@ -3,10 +3,12 @@ from email.header import Header
 from email.mime.text import MIMEText
 
 
-def send_mail(fromAddress, to, subject, text, username, password, host):
+def send_mail(fromAddress, to, subject, text, username, password, host, port):
     msg = MIMEText(text, _charset='UTF-8')
     msg['Subject'] = Header(subject, 'utf-8')
     msg['From'] = fromAddress
     msg['To'] = to
-    server = smtplib.SMTP(host)
+    server = smtplib.SMTP(host, port)
+    if username:
+        server.login(username, password)
     server.sendmail(fromAddress, [to], msg.as_bytes())
