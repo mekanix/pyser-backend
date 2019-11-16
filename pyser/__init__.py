a6072c5b2a3ad12cb9547fdf969d2cea86593a26
Goran Mekić <meka@tilda.center>
2019-11-16 14:16:52 +0100

    Handle CORS and credentials

--- pyser/__init__.py
+++ pyser/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_file
 from flask_collect import Collect
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_security import PeeweeUserDatastore, Security
 
@@ -34,5 +35,6 @@ def create_app(config, app=None):
     app.security = Security(app, app.user_datastore)
     app.jwt = JWTManager(app)
     create_api(app)
+    app.cors = CORS(app, supports_credentials=True)
 
     return app
